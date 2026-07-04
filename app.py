856d4e5c3908db56fd4e3faa961e4fbc3fadadb5
Abhishek11512 <jaina4281@gmail.com>
2026-07-04 14:28:15 +0530

    Initial Commit - Setup backend Infrastructure and layout

---
+++ app.py
@@ -0,0 +1,60 @@
+import sqlite3
+import requests 
+from flask import Flask, render_template, request, jsonify 
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/api/board', methods=['GET'])
+def get_board_data():
+    con = sqlite3.connect('tasks.db')
+    cr = con.cursor()
+    cr.execute('''SELECT id, title, status FROM tasks''')
+    all_board_data = cr.fetchall()
+    con.close()
+
+    local_task_list = [{"id": task[0], "title": task[1], "status": task[2]} for task in all_board_data]
+
+    repo_url = "https://github.com"
+    headers = {"User-Agent": "Kanban-App"}
+
+    github_projects_list = []
+
+    try:
+        response = requests.get(repo_url, headers=headers)
+        all_repos = response.json()
+
+        for repo in all_repos:
+            repo_name = repo.get("name")
+
+            topics_url = f"https://github.com{repo_name}/topics"
+            topics_res = requests.get(topics_url, headers=headers)
+            topics_list = topics_res.json().get('names', [])
+
+        if "completed" in topics_list:
+            tracker_status = "completed"
+        elif "Onhold" in topics_list: 
+            tracker_status = "Onhold"
+        else:
+            tracker_status = "Ongoing"
+    
+        github_projects_list.append({
+            "name": repo_name.replace("_", " ").title(),
+            "status": tracker_status
+        })
+    except Exception:
+        github_projects_list=[]
+
+    master_payload = {
+        "local_tasks": local_task_list,
+        "github_projects": github_projects_list
+    }
+    return jsonify(master_payload)
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+       
---
+++ init_db.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+con = sqlite3.connect('tasks.db')
+cr = con.cursor()
+
+cr.execute('''CREATE TABLE IF NOT EXISTS tasks (
+           id INTEGER PRIMARY KEY AUTOINCREMENT,
+           title VARCHAR(100),
+           status VARCHAR(30))''')
+
+con.commit()
+con.close()
+print("DATABASE CREATED SUCCESSFULLY!")
