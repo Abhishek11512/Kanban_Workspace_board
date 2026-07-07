3395ce0df2dcf09dca9bd82c262fe25c175c8c1e
Abhishek11512 <jaina4281@gmail.com>
2026-07-07 16:02:32 +0530

    1

--- init_db.py
+++ init_db.py
@@ -4,9 +4,9 @@ con = sqlite3.connect('tasks.db')
 cr = con.cursor()
 
 cr.execute('''CREATE TABLE IF NOT EXISTS tasks (
-           id INTEGER PRIMARY KEY AUTOINCREMENT,
-           title VARCHAR(100),
-           status VARCHAR(30))''')
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+title VARCHAR(100),
+status VARCHAR(30))''')
 
 con.commit()
 con.close()
